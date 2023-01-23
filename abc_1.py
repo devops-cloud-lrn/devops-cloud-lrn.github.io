2247e762513877e011606a3c4aa97ae5075af284
Saibaba Devotee <nifty.investments@gmail.com>
2023-01-22 23:54:45 -0600

    adding postgresql documentation

---
+++ abc_1.py
@@ -0,0 +1,6 @@
+import random
+#define a function to generate a random number
+def generate_random_number():
+    return random.randint(1, 100)
+
+print(f"The random value is {generate_random_number()}")
